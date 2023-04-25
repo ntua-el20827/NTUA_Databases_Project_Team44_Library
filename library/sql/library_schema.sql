909f2c6cbf67651b45e253d95e604a295f92b670
George <georgegeo248@gmail.com>
2023-04-25 14:19:49 +0300

    λαθος στα library_schema

--- library/sql/library_schema.sql
+++ library/sql/library_schema.sql
@@ -6,7 +6,14 @@ USE library;
 ---
 --- Tables
 ---
-
+CREATE TABLE actor (
+  actor_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(45) NOT NULL,
+  last_name VARCHAR(45) NOT NULL,
+  last_update TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY  (actor_id),
+  KEY idx_actor_last_name (last_name)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --- Table 'school'
 CREATE TABLE school (
