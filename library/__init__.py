dfd1699330d8a30e8ac78dc5e40ceb6ec8d0310c
George <georgegeo248@gmail.com>
2023-05-01 20:41:42 +0300

    DBMS connected with app

--- library/__init__.py
+++ library/__init__.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-app.config["MYSQL_USER"] = 'root'
-app.config["MYSQL_PASSWORD"] = ''
-app.config["MYSQL_DB"] = 'mydb'
-app.config["MYSQL_HOST"] = '127.0.0.1'
-app.config["SECRET_KEY"] = 'eee' ## secret key for sessions (signed cookies). Flask uses it to protect the contents of the user session against tampering.
+app.config["DB_ADMIN_USERNAME"] = 'root'
+app.config["DB_ADMIN_PASSWORD"] = 'george'
+app.config["DB_ADMIN_FIRSTNAME"] = 'MANOS'
+app.config["DB_ADMIN_LASTNAME"] = 'THELO KOYREMA'
+
+# Αυτά συσχετίζουν την βάση με την εφαρμογή που φτιάξαμε με το Flask
+# Δεν ξέρω ακριβώς πως λειτουργούν
+app.config['MYSQL_HOST'] = '127.0.0.1' # μαλλον το IP address την εφαρμογής
+app.config['MYSQL_USER'] = 'root' 
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'library'
+app.config["SECRET_KEY"] = 'ggg' ## secret key for sessions (signed cookies). Flask uses it to protect the contents of the user session against tampering.
+
+mysql = MySQL(app)
 
 from library import routes
---
+++ library/sql/insert_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO school (school_name, city, street, postal_code, email, principal_lastname, principal_firstname, school_admin_lastname, school_admin_firstname)
+VALUES ('Example School', 'Example City', 'Example Street 123', 12345, 'example@email.com', 'Smith', 'John', 'Doe', 'Jane');
