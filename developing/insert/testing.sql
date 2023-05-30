969e3a3f4b7d70f0df659bdf78259aa8db1b5aa3
George <georgegeo248@gmail.com>
2023-05-30 17:54:18 +0300

    ok

---
+++ developing/insert/testing.sql
@@ -0,0 +1,5 @@
+INSERT INTO book_status (book_id, user_id, status, request_date, approval_date, return_date)
+VALUES (1, 15, 'borrowed', '2023-05-26', '2023-05-27', '2023-05-29');
+
+INSERT INTO book_status (book_id, user_id, status, request_date)
+VALUES (1, 15, 'reserved', '2023-05-30');
