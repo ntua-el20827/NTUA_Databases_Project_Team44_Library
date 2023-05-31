8c5ed519bacdfa79750b570129b9acde308069ec
Manos Neofotistos <neofotistosmanos@gmail.com>
2023-05-31 20:24:09 +0300

    manos

--- library/routes.py
+++ library/routes.py
@@ -342,20 +342,20 @@ def mybooks():
         cur.close()
 
     cur = mydb.connection.cursor()
-    query = """ SELECT bs.book_id, b.title, b.language, b.ISBN
+    query = """ SELECT bs.book_id, b.title, b.book_language, b.ISBN
 FROM book_status bs
 JOIN book b ON bs.book_id = b.book_id
 WHERE bs.user_id = %s AND bs.status = 'borrowed' """
 
     cur.execute(query,(user_id,))
     borrowed_books = cur.fetchall()
-    query = """ SELECT bs.book_id, b.title, b.language, b.ISBN
+    query = """ SELECT bs.book_id, b.title, b.book_language, b.ISBN
 FROM book_status bs
 JOIN book b ON bs.book_id = b.book_id
 WHERE bs.user_id = %s AND bs.status = 'reerved' """
     cur.execute(query,(user_id,))
     reserved_books = cur.fetchall()
-    query = """ SELECT bs.book_id, b.title,b.language, b.ISBN
+    query = """ SELECT bs.book_id, b.title,b.book_language, b.ISBN
 FROM book_status bs
 JOIN book b ON bs.book_id = b.book_id
 WHERE bs.user_id = %s AND bs.status = 'queue' """
